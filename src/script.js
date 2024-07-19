4a9d667859280c768d9344e4396d8ee4f647d7ae
nathan tang <nathan.tang@ecn.forces.gc.ca>
2024-07-19 17:09:10 -0400

    Code Restructure and Game World Started
    - basic plane for game world and model added
    - code restructuring to use classes for world dev
    - more assets added

--- src/script.js
+++ src/script.js
@@ -1,53 +1,105 @@
 import * as THREE from 'three';
-/**
- * Base
- */
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //Canvas
 const canvas = document.querySelector('canvas.webgl');
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-};
+class CharControl {
 
-//Scene
-const scene = new THREE.Scene();
+}
 
-//Camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100);
-scene.add(camera);
+class World {
+    constructor() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
 
-//Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas
-});
-renderer.setSize(sizes.width, sizes.height);
+        this.create();
+    }
 
-//resizing window
-window.addEventListener('resize', (e) => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+    create() {
+        //scene
+        this.scene = new THREE.Scene();
 
-    camera.aspect = sizes.width/sizes.height;
-    camera.updateProjectionMatrix();
+        //camera
+        this.camera = new THREE.PerspectiveCamera(50, this.width/this.height, 1, 1000);
+        this.camera.position.set(80, 10, 0);
 
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+        //renderer
+        this.renderer = new THREE.WebGLRenderer({
+            canvas
+        });
+        this.renderer.setSize(this.width, this.height);
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.BasicShadowMap;
 
+        const dirLight = new THREE.DirectionalLight(0xFFFFFF);
+        dirLight.position.set(100, 100, 100);
+        dirLight.castShadow = true;
+        dirLight.shadow.mapSize.width = 2048;
+        dirLight.shadow.mapSize.length = 2048;
+        dirLight.shadow.camera.near = 1;
+        dirLight.shadow.camera.far = 500;
+        dirLight.shadow.camera.left = 200;
+        dirLight.shadow.camera.right = -200;
+        dirLight.shadow.camera.top = 200;
+        dirLight.shadow.camera.bottom = -200;
+        this.scene.add(dirLight);
 
-/**
- * Animate
- */
-const clock = new THREE.Clock();
+        const ambLight = new THREE.AmbientLight(0x404040, 5);
+        this.scene.add(ambLight);
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
+        //create player ground
+        const plane = new THREE.Mesh(
+            new THREE.PlaneGeometry(100,100,1,1),
+            new THREE.MeshStandardMaterial({
+                color: 0xFFFFFF
+            })
+        );
+        plane.castShadow = false;
+        plane.receiveShadow = true;
+        plane.rotation.x = -Math.PI / 2;
+        this.scene.add(plane)
 
-    renderer.render(scene, camera);
+        const controls = new OrbitControls(this.camera, canvas)
+        controls.enableDamping = true;
+        
+        this.createPlayer();
+        this.tick();
+    }
 
-    window.requestAnimationFrame(tick);
-};
+    createPlayer() {
+        const mdlLoader = new GLTFLoader();
+        mdlLoader.load(
+            'models/capybara.glb',
+            (gltf) => {
+                gltf.scene.traverse((layer) => {
+                    layer.castShadow = true;
+                });
+                this.scene.add(gltf.scene);
+            }
+        );
+    }
 
-tick();
+    windowResize() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+
+        this.camera.aspect = this.width/this.height;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(this.width, this.height);
+    }
+
+    tick() {
+        let stamp = requestAnimationFrame(() => {
+            this.renderer.render(this.scene, this.camera);
+            this.tick();
+        })
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    let gameWorld = new World();
+  });
